f10e3fb8e870e168d3ad282688d484fb1cd1acff
Bilal Rasheed <awan.igd@gmail.com>
2026-09-01 20:55:28 +0500

    Commite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 
 android {
     namespace = "reader.aigd"
-    compileSdk = 34
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "reader.aigd"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "3"
     }
 
     buildTypes {
@@ -25,17 +25,21 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+
 
 
 }
 
 dependencies {
+
     implementation("com.startapp:inapp-sdk:5.3.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.activity.ktx)
+    implementation(libs.constraintlayout)
 }
